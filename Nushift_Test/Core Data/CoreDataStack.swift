1a1d7cdc8bc522e136e143af5f88d61879d17a09
Sreekanth <ios.sreekanth@gmail.com>
2024-08-25 21:57:45 +0530

    Data base integration

---
+++ Nushift_Test/Core Data/CoreDataStack.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataStack.swift
+//  Nushift_Test
+//
+//  Created by SREEKANTH on 25/08/24.
+//
+
+import Foundation
---
+++ Nushift_Test/Core Data/DataBaseLayer.swift
@@ -0,0 +1,8 @@
+//
+//  DataBaseLayer.swift
+//  Nushift_Test
+//
+//  Created by SREEKANTH on 25/08/24.
+//
+
+import Foundation
